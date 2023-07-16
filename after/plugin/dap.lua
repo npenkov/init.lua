d1247e587a50bca9cca22cae16dcb80bb499ea4a
Nick Penkov <nick.penkov@gmail.com>
2023-07-16 17:11:15 +0200

    Open DAP UI on DAP start/Close on terminate.

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -74,3 +74,15 @@ vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='DapBreakpoint
 vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
 vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
 vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
+
+-- DAP events -> DAP UI
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
