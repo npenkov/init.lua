4ca0ad71cec684434e922ad325903249b5d79b5d
Nick Penkov <nick.penkov@gmail.com>
2023-08-08 22:34:01 +0200

    Add markdown preview

--- lua/npenkov/packer.lua
+++ lua/npenkov/packer.lua
@@ -132,4 +132,13 @@ return require('packer').startup(function(use)
       end
   }
   use 'simrat39/rust-tools.nvim'
+
+  -- Markdown preview
+  -- install without yarn or npm
+  use({
+      "iamcco/markdown-preview.nvim",
+      run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
+  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 end)
